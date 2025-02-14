32677e6e9286be03c3cbe49f9fe80cd006d2c1e7
Yulian505 <yuliancanul5@gmail.com>
2025-02-14 09:01:55 -0600

    Starting TaBarView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,9 @@
+import 'package:donut_app_2a_marin1/tabs/burger_tab.dart';
+import 'package:donut_app_2a_marin1/tabs/donut_tab.dart';
+import 'package:donut_app_2a_marin1/tabs/pancakes_tab.dart';
+import 'package:donut_app_2a_marin1/tabs/pizza_tab.dart';
+import 'package:donut_app_2a_marin1/tabs/smoothie_tab.dart';
+import 'package:donut_app_2a_marin1/utils/my_tab.dart';
 import 'package:flutter/material.dart';
  
 class HomePage extends StatefulWidget {
@@ -8,49 +14,76 @@ class HomePage extends StatefulWidget {
 }
  
 class _HomePageState extends State<HomePage> {
+
+  List<Widget> myTabs = [
+    //Donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //Burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    //Smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    //Pancake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    //Pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
+   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        //Ícono de la Izquierda
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800],
-        ),
-        actions: [Padding(
-          padding: const EdgeInsets.only(right: 24.0),
-          child: Icon(Icons.person),
-        )
-        ],
-      ),
-      body: const Column(
-        children: [
-          //Texto "I want to Eat"
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-            child: Row(
-              children: [
-                Text("I want to ",
-                style: TextStyle(fontSize: 32)),
-                Text("Eat",
-                style: TextStyle(
-                  //Tamaño de letra
-                  fontSize: 32,
-                  //Negritas
-                  fontWeight: FontWeight.bold,
-                  //Subrayado
-                  decoration: TextDecoration.underline)
-                ),
-              ],
-            ),
+    return DefaultTabController(
+      length: myTabs.length,
+      //Este widget sirve para gestionar las pestañas
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          //Ícono de la Izquierda
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
+          ),
+          actions: [Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
           )
-          //Tap bar
- 
-          //Tab bar View
- 
-          //Carrito
-        ],)
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto "I want to Eat"
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to ",
+                  style: TextStyle(fontSize: 32)),
+                  Text("Eat",
+                  style: TextStyle(
+                    //Tamaño de letra
+                    fontSize: 32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline)
+                  ),
+                ],
+              ),
+            ),
+            //Tap bar (Pestañas)
+            TabBar(tabs: myTabs),
+      
+            //Tab bar View(Contenido de pestañas)
+            Expanded(
+              child: TabBarView(children: [
+                DonutTab(),
+                BurgerTab(),
+                SmoothieTab(),
+                PancakesTab(),
+                PizzaTab(),
+              ],)
+            )
+            //Carrito
+          ],)
+      ),
     );
   }
 }
