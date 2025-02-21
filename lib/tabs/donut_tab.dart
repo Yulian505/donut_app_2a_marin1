8423b300b99ac6a19918c8726db795b856dd84be
Yulian505 <yuliancanul5@gmail.com>
2025-02-21 08:48:41 -0600

    starding GridView

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../utils/donut_tile.dart';
+
 class DonutTab extends StatelessWidget {
-  const DonutTab({super.key});
+
+  //lista de donas
+  final List donutsOnSale = [
+      // [ donutFlavor, donutPrice, donutColor, imageName ]
+   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+
+  ];
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Donut Tab"),
-    );
+    return GridView.builder(
+      //Cuantos elementos tiene
+      itemCount: donutsOnSale.length,
+      padding: const EdgeInsets.all(12),
+      //Encargado de organizar la cuadrícula
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //Widget desplazable y eficiente para listar y encabezados
+
+        //Determina el número de columnas
+        crossAxisCount: 2),
+      itemBuilder: (context, index){
+        return DonutTile(
+          //Elemento individual de la cuadrícula
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
+      });
+    
   }
 }
